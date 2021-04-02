14c9c4d4e6aef685cdd95d99c54bcb625b92ba83
Vladimir Chernyshev <vladimic@microsoft.com>
2021-04-02 10:50:20 -0700

    Improving output somewhat

--- 01/aoc2020_01/src/main/scala/Main.scala
+++ 01/aoc2020_01/src/main/scala/Main.scala
@@ -5,19 +5,23 @@ object Main extends App {
   val lines = bufferedSource.getLines().toArray.map(_.toInt)
   bufferedSource.close
 
+  var result = 0
   for (line1 <- lines) {
     for (line2 <- lines) {
       if (line1 + line2 == 2020)
-        println(line1 * line2)
+        result = line1 * line2
     }
   }
 
-    for (line1 <- lines) {
+  println(result)
+
+  for (line1 <- lines) {
     for (line2 <- lines) {
       for (line3 <- lines) {
-      if (line1 + line2 + line3 == 2020)
-        println(line1 * line2 * line3)
+        if (line1 + line2 + line3 == 2020)
+          result = line1 * line2 * line3
       }
     }
   }
+  println(result)
 }
