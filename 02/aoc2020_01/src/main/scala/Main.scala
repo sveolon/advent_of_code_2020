cedf0c5945c09ae711d1bba2ccffcffcf645d474
Vladimir Chernyshev <vladimic@microsoft.com>
2021-04-07 19:11:08 -0700

    02_2

--- 02/aoc2020_01/src/main/scala/Main.scala
+++ 02/aoc2020_01/src/main/scala/Main.scala
@@ -1,15 +1,19 @@
 import scala.io.Source
 
 object Main extends App {
-  def validate(item: (Int, Int, Char, String)): Int = {
+  def validate2(item: (Int, Int, Char, String)): Int = {
+    if (item._4.charAt(item._1-1) == item._3 ^ item._4.charAt(item._2-1) == item._3) 1 else 0
+  }
+  
+  def validate1(item: (Int, Int, Char, String)): Int = {
     val c = item._4.count(_ == item._3)
     if (item._1 <= c && c <= item._2) 1 else 0
   }
 
-  def count(list: List[(Int, Int, Char, String)]): Int = {
+  def count(list: List[(Int, Int, Char, String)], fun: ((Int, Int, Char, String)) => Int): Int = {
     list match {
       case Nil => 0
-      case head :: next => validate(head) + count(next)
+      case head :: next => fun(head) + count(next, fun(_))
     }
   }
 
@@ -22,7 +26,8 @@ object Main extends App {
       case Array(f, t, c, s1, s2) => (f.toInt, t.toInt, c.toCharArray()(0), s2)
     }).toList
     
-    println(count(list))
+    println(count(list, validate1(_)))
+    println(count(list, validate2(_)))
   }
   run
 }
