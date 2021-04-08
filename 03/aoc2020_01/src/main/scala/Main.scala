636615f9aecfd87abcd1139dd071951b9f394b1b
Vladimir Chernyshev <vladimic@microsoft.com>
2021-04-08 15:09:38 -0700

    03_2

--- 03/aoc2020_01/src/main/scala/Main.scala
+++ 03/aoc2020_01/src/main/scala/Main.scala
@@ -5,19 +5,30 @@ object Main extends App {
     if (l(y) == '#') 1 else 0
   }
 
-  def count(lines: List[String], y: Int): Int = {
-    lines match {
-      case Nil => 0
-      case head :: next => check(head, y) + count(next, (y + 3) % head.length())
+  def count(lines: Array[String], yInc: Int, xInc: Int): Long = {
+    var result = 0
+    var curX = 0
+    var curY = 0
+    while (curX < lines.length) {
+      result += check(lines(curX), curY)
+      curY = (curY + yInc) % lines(curX).length()
+      curX += xInc
     }
+    result
   }
 
   def run {
     val bufferedSource = Source.fromFile("in.txt")
-    val lines = bufferedSource.getLines().toList
+    val lines = bufferedSource.getLines().toArray
     bufferedSource.close
 
-    println(count(lines, 0))
+    println(
+      count(lines, 1, 1) *
+      count(lines, 3, 1) *
+      count(lines, 5, 1) *
+      count(lines, 7, 1) *
+      count(lines, 1, 2)
+    )
   }
   run
 }
